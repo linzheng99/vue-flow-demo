6493a27ee7f97b4fa69a67f8d6a677637d36eeec
linzheng <alidlg9045@126.com>
2025-02-26 23:29:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="size-full">
+    <DragCanvas />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DragCanvas from './components/dragCanvas/index.vue'
+
+</script>
+
+<style scoped>
+
+</style>
